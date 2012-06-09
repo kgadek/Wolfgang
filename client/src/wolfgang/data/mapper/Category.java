97171a4773caf2ab782a450413386677eb15e041
Konrad Gądek <kgadek@gmail.com>
2012-06-09 20:51:37 +0200

    Pociągwork

--- client/src/wolfgang/data/mapper/Category.java
+++ client/src/wolfgang/data/mapper/Category.java
@@ -1,52 +1,48 @@
 package wolfgang.data.mapper;
 
-public class Category extends _TableEntry {
-	public final int id;
-	public final String description;
-	public final int balance;
-	public final Boolean accumulator;
+public class Category {
+    public final int id;
+    public final String description;
+    public final int balance;
+    public final Boolean accumulator;
 
-	public Category(Category c) {
-		super();
-		this.id = c.id;
-		this.description = c.description;
-		this.balance = c.balance;
-		this.accumulator = c.accumulator;
-	}
-	public Category(int id, String description, int balance, Boolean accumulator) {
-		super();
-		this.id = id;
-		this.description = description;
-		this.balance = balance;
-		this.accumulator = accumulator;
-	}
-	
-	public Category setId(int newId) {
-		if(newId == id)
-			return this;
-		Category ret = new Category(newId, description, balance, accumulator);
-		ret.setModified();
-		return ret;
-	}
-	public Category setDescription(String newDescription) {
-		if(newDescription == null)
-			throw new NullPointerException();
-		if(newDescription.equals(description))
-			return this;
-		Category ret = new Category(id, newDescription, balance, accumulator);
-		ret.setModified();
-		return ret;
-	}
-	public Category setBalance(int newBalance) {
-		if(newBalance == balance)
-			return this;
-		Category ret = new Category(id, description, newBalance, accumulator);
-		ret.setModified();
-		return ret;
-	}
-	public Category setAccumulator(boolean newAccumulator) {
-		Category ret = new Category(id, description, balance, newAccumulator);
-		ret.setModified();
-		return ret;
-	}
+    public Category(Category c) {
+        super();
+        this.id = c.id;
+        this.description = c.description;
+        this.balance = c.balance;
+        this.accumulator = c.accumulator;
+    }
+    public Category(int id, String description, int balance, Boolean accumulator) {
+        super();
+        this.id = id;
+        this.description = description;
+        this.balance = balance;
+        this.accumulator = accumulator;
+    }
+
+    public Category setId(int newId) {
+        if(newId == id)
+            return this;
+        Category ret = new Category(newId, description, balance, accumulator);
+        return ret;
+    }
+    public Category setDescription(String newDescription) {
+        if(newDescription == null)
+            throw new NullPointerException();
+        if(newDescription.equals(description))
+            return this;
+        Category ret = new Category(id, newDescription, balance, accumulator);
+        return ret;
+    }
+    public Category setBalance(int newBalance) {
+        if(newBalance == balance)
+            return this;
+        Category ret = new Category(id, description, newBalance, accumulator);
+        return ret;
+    }
+    public Category setAccumulator(boolean newAccumulator) {
+        Category ret = new Category(id, description, balance, newAccumulator);
+        return ret;
+    }
 }
--- client/src/wolfgang/data/mapper/User.java
+++ client/src/wolfgang/data/mapper/User.java
@@ -4,61 +4,60 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
-public class User extends _TableEntry {
-	public final int id;
-	public final String login;
-	public final Date lastLogin;
-	public final byte[] passwordHash;
-	
-	public User(User u) {
-		super();
-		this.id = u.id;
-		this.login = u.login;
-		this.lastLogin = u.lastLogin;
-		this.passwordHash = u.passwordHash;
-	}
-	public User(int id, String login, Date lastLogin, String passwordHash) {
-		super();
-		this.id = id;
-		this.login = login;
-		this.lastLogin = lastLogin;
-		this.passwordHash = passwordHash.getBytes();
-	}
-	
-	public User setId(int newId) {
-		if(newId == id)
-			return this;
-		User ret = new User(newId, login, lastLogin, passwordHash.toString());
-		ret.setModified();
-		return ret;
-	}
-	public User setLogin(String newLogin) {
-		if(newLogin == null)
-			throw new NullPointerException();
-		if(newLogin.equals(login))
-			return this;
-		User ret = new User(id, newLogin, lastLogin, passwordHash.toString());
-		ret.setModified();
-		return ret;
-	}
-	public User setLastLogin(Date newLastLogin) {
-		if(newLastLogin == null)
-			throw new NullPointerException();
-		if(newLastLogin == lastLogin)
-			return this;
-		User ret = new User(id, login, newLastLogin, passwordHash.toString());
-		ret.setModified();
-		return ret;
-	}
-	public User setPassword(String newPassword) throws NoSuchAlgorithmException {
-		if(newPassword == null)
-			throw new NullPointerException();
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		md.update(newPassword.getBytes());
-		byte[] newPasswordHash = md.digest();
-		// always return new, no matter this is the same pass
-		User ret = new User(id, login, lastLogin, newPasswordHash.toString());
-		ret.setModified();
-		return ret;
-	}
+import wolfgang.data.DataMaster;
+
+public class User {
+    public final int id; // ustalane przez applikację, zero gdy nie wiadomo
+    public final String login;
+    public final Date lastLogin;
+    public final byte[] passwordHash;
+
+    public User(User u) {
+        super();
+        this.id = u.id;
+        this.login = u.login;
+        this.lastLogin = u.lastLogin;
+        this.passwordHash = u.passwordHash;
+    }
+    public User(int id, String login, Date lastLogin, String passwordHash) {
+        super();
+        this.id = id;
+        this.login = login;
+        this.lastLogin = lastLogin;
+        this.passwordHash = passwordHash.getBytes();
+    }
+
+    public User setId(int newId) {
+        if(newId == id)
+            return this;
+        User ret = new User(newId, login, lastLogin, passwordHash.toString());
+        return ret;
+    }
+    public User setLogin(String newLogin) {
+        if(newLogin == null)
+            throw new NullPointerException();
+        if(newLogin.equals(login))
+            return this;
+        User ret = new User(id, newLogin, lastLogin, DataMaster.bytesToString(passwordHash));
+        return ret;
+    }
+    public User setLastLogin(Date newLastLogin) {
+        if(newLastLogin == null)
+            throw new NullPointerException();
+        if(newLastLogin == lastLogin)
+            return this;
+        User ret = new User(id, login, newLastLogin, passwordHash.toString());
+        return ret;
+    }
+    public User setPassword(String newPassword) throws NoSuchAlgorithmException {
+        DataMaster.logger.entering(this.getClass().getName(), "setPassword");
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(newPassword.getBytes());
+        byte[] newPasswordHash = md.digest();
+        DataMaster.logger.finest("SHA256("+newPassword+") = "+DataMaster.bytesToString(newPasswordHash));
+        // always return new, no matter this is the same pass
+        User ret = new User(id, login, lastLogin, DataMaster.bytesToString(newPasswordHash));
+        DataMaster.logger.exiting(this.getClass().getName(), "setPassword");
+        return ret;
+    }
 }
